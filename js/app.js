d85cb7698b1aa877e63d0819a5c75465f790a490
HARSH RAJ <harshraj250106@gmail.com>
2025-06-29 00:42:17 +0530

    29-06-25

--- js/app.js
+++ js/app.js
@@ -116,3 +116,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+
+
+  const sections = document.querySelectorAll("section[id], div[id]");
+  const navLinks = document.querySelectorAll(".main-nav-link");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").substring(1) === entry.target.id) {
+              link.classList.add("active");
+            }
+          });
+        }
+      });
+    },
+    {
+      threshold: 0.6, // Adjust for sensitivity
+    }
+  );
+
+  sections.forEach((section) => observer.observe(section));
+
